a88c7d31c6b0911dfce352b2ae97f61051998b2b
carlos.junior <carlos.udi@live.com>
2025-07-04 15:38:55 -0300

    Update code
    Create TransactionHistoryActivity.kt

---
+++ app/src/main/java/com/carlosmagno/mywallet/presentation/history/TransactionHistoryActivity.kt
@@ -0,0 +1,35 @@
+package com.carlosmagno.mywallet.presentation.history
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.carlosmagno.mywallet.databinding.ActivityTransactionHistoryBinding
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+
+class TransactionHistoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTransactionHistoryBinding
+    private val viewModel: TransactionHistoryViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTransactionHistoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvTransactions.layoutManager = LinearLayoutManager(this)
+
+        val userId = intent.getIntExtra("USER_ID", -1)
+
+        viewModel.loadTransactions(userId)
+
+        lifecycleScope.launch {
+            viewModel.transactions.collectLatest { list ->
+                binding.rvTransactions.adapter = TransactionsAdapter(list)
+            }
+        }
+    }
+}
